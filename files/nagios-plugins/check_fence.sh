8e2c486de965ffce4943cbfe01e4895e2430a746
frvdamme <frank.van.damme@vub.ac.be>
2016-09-16 15:58:24 +0200

    fencing plugin: check for clone instead of primitive

--- files/nagios-plugins/check_fence.sh
+++ files/nagios-plugins/check_fence.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 host=$1
-fence_res_out="$(crm resource status p_fence_$host 2>&1)"
+fence_res_out="$(crm resource status clone_fence_$host 2>&1)"
 echo -n "FENCE_$host "
 
-echo $fence_res_out | grep -q "^resource p_fence_${host} is running on: "
+echo $fence_res_out | grep -q "^resource clone_fence_${host} is running on: "
 running=$?
 
 if [[  $running -eq 0 ]]
 then
     echo OK: running
 else
-    if [[ "$fence_res_out" == "resource p_fence_"${host}" is NOT running" ]]
+    if [[ "$fence_res_out" == "resource clone_fence_"${host}" is NOT running" ]]
     then
         echo CRITICAL: $fence_res_out
         exit 2
